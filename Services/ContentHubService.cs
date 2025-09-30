d386a46a7aaa3d1cb9f63742fd83ef6011a86ca5
Kumari, Archana <Archana.Kumari@meritagehomes.com>
2025-10-01 00:46:15 +0530

    add the for loop and all relation fields

--- Services/ContentHubService.cs
+++ Services/ContentHubService.cs
@@ -18,27 +18,77 @@
         public async Task<List<ImportResult>> ImportAsync(List<GalleryItem> items)
         {
             var results = new List<ImportResult>();
-            var definition = await _mClient.EntityDefinitions.GetAsync("MH.AssetGallery");
-            Console.WriteLine($"Definition: {definition.Name}");
-            foreach (var member in definition.MemberGroups)
+            foreach (var item in items)
             {
-                Console.WriteLine($"{member.Name} (type: {member.MemberDefinitions})");
+                var entity = await _mClient.EntityFactory.CreateAsync("MH.AssetGallery");
+                entity.SetPropertyValue("Title", item.galleryTitle);
+                entity.SetPropertyValue("Description", item.galleryDescription);
+                foreach (var relatedmetroarea in item.relatedMetroAreas)
+                {
+                    var metroArea = await _mClient.Entities.GetAsync(relatedmetroarea.contentHubEntityIdentifier, new EntityLoadConfiguration
+                    {
+                        RelationLoadOption = RelationLoadOption.All
+                    });
+                    var galleriesToMetroAreas = entity.GetRelation<IChildToManyParentsRelation>("GalleriesToMetroAreas");
+                    if (metroArea?.Id != null)
+                    {
+                        galleriesToMetroAreas.Add(metroArea.Id.Value);
+                    }
+                }
+                foreach (var lotItem in item.relatedLots)
+                {
+                    var lot = await _mClient.Entities.GetAsync(lotItem.contentHubEntityIdentifier, new EntityLoadConfiguration
+                    {
+                       RelationLoadOption = RelationLoadOption.All
+                    });
+                    var galleriesToLots = entity.GetRelation<IChildToManyParentsRelation>("GalleriesToLots");
+                    if (lot?.Id != null)
+                    {
+                        galleriesToLots.Add(lot.Id.Value);
+                    }
+                }
+                foreach (var floorplanItem in item.relatedFloorplans)
+                {
+                    var floorplan = await _mClient.Entities.GetAsync(floorplanItem.contentHubEntityIdentifier, new EntityLoadConfiguration
+                    {
+                        RelationLoadOption = RelationLoadOption.All
+                    });
+                    var galleriesToPlans = entity.GetRelation<IChildToManyParentsRelation>("GalleriesToPlans");
+                    if (floorplan?.Id != null)
+                    {
+                        galleriesToPlans.Add(floorplan.Id.Value);
+                    }
+                }
+                foreach (var relatedcommunitysheet in item.relatedCommunitySheets)
+                {
+                    var communitySheet = await _mClient.Entities.GetAsync(relatedcommunitysheet.contentHubEntityIdentifier, new EntityLoadConfiguration
+                    {
+                        RelationLoadOption = RelationLoadOption.All
+                    });
+                    var galleriesToCommunitySheets = entity.GetRelation<IChildToManyParentsRelation>("GalleriesToCommunitySheets");
+                    if (communitySheet?.Id != null)
+                    {
+                        galleriesToCommunitySheets.Add(communitySheet.Id.Value);
+                    }
+                }
+                //foreach (var sliedItem in item.slides)
+                //{
+                //    var slide = await _mClient.Entities.GetAsync(sliedItem.slideTitle, new EntityLoadConfiguration
+                //    {
+                //        RelationLoadOption = RelationLoadOption.All
+                //    });
+                //    var assetGalleriesToAssetGallerySlides = entity.GetRelation<IParentToManyChildrenRelation>("AssetGalleriesToAssetGallerySlides");
+                //    if (slide?.Id != null)
+                //    {
+                //        assetGalleriesToAssetGallerySlides.Add(slide.Id.Value);
+                //    }
+                //}
+                var savedEntity = await _mClient.Entities.SaveAsync(entity);
+                break;
+
             }
-            var entity = await _mClient.EntityFactory.CreateAsync("MH.AssetGallery");
-            entity.SetPropertyValue("Title","TestGalleryMigration");
 
-            var metroArea = await _mClient.Entities.GetAsync("ux8xh4j1pUGBz8Iw6TNTpw", new EntityLoadConfiguration
-            {
-                RelationLoadOption = RelationLoadOption.All
-            });
 
-            var galleriesToMetroAreas = entity.GetRelation< IChildToManyParentsRelation>("GalleriesToMetroAreas");
-            if (metroArea?.Id != null)
-            {
-                galleriesToMetroAreas.Add(metroArea.Id.Value);
-            }
-
-            var savedEntity = await _mClient.Entities.SaveAsync(entity);
 
             return results;
         }
